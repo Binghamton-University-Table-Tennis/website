ca7bbe81639cc636f6f5573aeb7981b5510a55fc
Jeffrey Chan <jchan63@binghamton.edu>
2016-09-11 21:50:23 +0000

    Changed main page to reflect table tennis

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Players(models.Model):
+    lastname = models.CharField(max_length=10)
+    firstname = models.CharField(max_length=10)
+    rating = models.IntegerField()
+    
+    class Meta:
+        db_table = "players"
+    
