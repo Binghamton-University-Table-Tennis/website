58f2cd3a489ce6e301287cf695b961505307c9a3
Jeffrey Chan <jchan63@binghamton.edu>
2016-09-13 03:19:09 +0000

    Updated comments to try to re-deploy to heroku

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,5 @@
 """
-Django settings for gettingstarted project, on Heroku. For more info, see:
-https://github.com/heroku/heroku-django-template
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.8/ref/settings/
+Django settings for rating-system project, on Heroku. 
 """
 
 import os
