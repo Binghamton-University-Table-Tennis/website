167c792ab84909790dd89fb85fc993d312e63df3
Jeffrey Chan <jchan63@binghamton.edu>
2016-09-13 18:32:37 +0000

    Removed unnecessary db params

--- config/settings.py
+++ config/settings.py
@@ -73,8 +73,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
