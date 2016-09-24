457957d5d256388f450a873d4eb54142af762a47
Jeffrey Chan <superjeffreyc.cs@gmail.com>
2016-09-24 00:23:00 +0000

    Switched database setting to use postgresql

--- config/settings.py
+++ config/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ubuntu',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
