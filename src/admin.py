fb889dc361c92158225b8241d65adfa37cf04f84
Jeffrey Chan <superjeffreyc.cs@gmail.com>
2016-09-28 05:16:58 +0000

    Removed auth forms from admin page

--- src/admin.py
+++ src/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 
 # Register your models here.
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 from .models import Players
 from .models import Matches
 
+admin.site.unregister(User)
+admin.site.unregister(Group)
 admin.site.register(Players)
 admin.site.register(Matches)
