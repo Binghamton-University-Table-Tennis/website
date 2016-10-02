c981aa29eb4accc701bc29187e6d4b53f027c9e4
Jeffrey Chan <superjeffreyc.cs@gmail.com>
2016-10-02 03:59:14 +0000

    Removed unused functions from views

--- src/views.py
+++ src/views.py
@@ -11,55 +11,6 @@ from .models import AttendanceHistory
 
 from Ratings import * 
 
-
-#updates player's stats only if they haven't been updated, function called before the page is rendered.
-#after an unaccounted match is taken care of towards a player's score, the updated is marked true in the match table stored in the database
-#this means the match's effect on a player's rating is put toward their overall rating once and only once
-def checkForUpdates():
-    #we extract all matches that haven't been put towards players' ratings, and sort them by the date the matches occurred
-    notYetUpdated = Matches.objects.all().filter(Updated = 0).order_by('Day')
-    #iterating over each match, we update the stats of the winner and loser for that match
-    for m in notYetUpdated:
-        winner = Players.objects.all().filter(First_Name = m.Winner_First_Name, Last_Name = m.Winner_Last_Name)
-        loser = Players.objects.all().filter(First_Name = m.Loser_First_Name, Last_Name = m.Loser_Last_Name)
-        win = 0
-        lose = 0
-        winPts = []
-
-        for w, l in zip(winner, loser):
-            win = w.Rating
-            lose = l.Rating
-            #call function that calculates how much the players' ratings should change, based on their difference in skill level
-            # Returns array: [Winner's new rating, Loser's new rating, Points exchanged]
-            winPts = calculateRatings(win, lose)
-
-            winnerMatchesPlayed = w.Matches_Played + 1
-            loserMatchesPlayed = l.Matches_Played + 1
-            winnerMatchesWon = w.Matches_Won + 1
-            loserMatchesWon = l.Matches_Won
-            
-            w.Rating = winPts[0]
-            l.Rating = winPts[1]
-            
-            w.Matches_Played += 1
-            l.Matches_Played += 1
-            
-            w.Matches_Won += 1
-            l.Matches_Lost += 1
-            
-            # Calculate win rate percentage- need to use floats to force floating point arithmetic
-            w.Win_Rate = int((float(winnerMatchesWon) / winnerMatchesPlayed)*100)
-            l.Win_Rate = int((float(loserMatchesWon) / loserMatchesPlayed)*100)
-            
-            w.save()
-            l.save()
-        
-        m.Points = winPts[2]
-        m.Winner_Rating = winPts[0]
-        m.Loser_Rating = winPts[1]
-        m.Updated = 1
-        m.save()
-
 # Create your views here.
 
 def about(request):
@@ -202,25 +153,6 @@ def getStanding(standing):
         classStanding = "Unknown"
     
     return classStanding
-        
-def getMedal(rating):
-    medal = ""
-    if rating < 1050:
-        medal = "Bronze"
-    elif rating < 1100:
-        medal = "Silver"
-    elif rating < 1150:
-        medal = "Gold"
-    elif rating < 1200:
-        medal = "Ruby"
-    elif rating < 1250:
-        medal = "Platinum"
-    elif rating < 1300:
-        medal = "Crystal"
-    else:
-        medal = "Diamond"
-        
-    return medal
     
 def search(request):
     result = request.GET.get('searchName')
