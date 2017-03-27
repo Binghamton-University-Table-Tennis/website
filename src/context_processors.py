38e57bbf05dc42d37f5914eaddbd0a0fc9bd58f7
Jeffrey Chan <jchan63@binghamton.edu>
2017-03-27 21:57:20 +0000

    Return a string from getPracticeLocation instead of an object

--- src/context_processors.py
+++ src/context_processors.py
@@ -1,11 +1,13 @@
 from .models import Location
+from .models import Images
+
 
 def getPracticeLocation(request):
 
     locationList = Location.objects.all()
 
     if len(locationList) == 1:
-        location = locationList[0]
+        location = locationList[0].Description
     else:
         location = "TBD"
 
