f7ff0b4c5590c13fb5f92a5682f377e2ebd06189
Jeffrey Chan <jchan63@binghamton.edu>
2017-03-28 21:15:16 +0000

    Increased textbox sizes in admin panel

--- src/admin.py
+++ src/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from django.db import models
+from django.forms import Textarea, TextInput
 
 # Register your models here.
 from django.contrib.auth.models import User
@@ -17,18 +18,32 @@ from .models import OrganizationInformation
 from .models import FrontPageContent
 from .models import SocialMedia
 
+class TextInputAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'150'})},
+    }
+
+class TextareaAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={
+                                'rows': 1,
+                                'cols': 150,
+                                'style': 'height: 5em;'
+
+        })},
+    }
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
 admin.site.register(Players)
-admin.site.register(Matches)
-admin.site.register(Updates)
+admin.site.register(Matches, TextInputAdmin)
+admin.site.register(Updates, TextareaAdmin)
 admin.site.register(ClubAttendance)
-admin.site.register(Slides)
+admin.site.register(Slides, TextInputAdmin)
 admin.site.register(EBoard)
-admin.site.register(Images)
+admin.site.register(Images, TextInputAdmin)
 admin.site.register(Practices)
-admin.site.register(AttendanceHistory)
-admin.site.register(OrganizationInformation)
-admin.site.register(FrontPageContent)
-admin.site.register(SocialMedia)
+admin.site.register(AttendanceHistory, TextInputAdmin)
+admin.site.register(OrganizationInformation, TextareaAdmin)
+admin.site.register(FrontPageContent, TextareaAdmin)
+admin.site.register(SocialMedia, TextareaAdmin)
