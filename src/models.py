034653ff9a2824daa27c8f08488ee6906faa6aba
Jeffrey Chan <jchan63@binghamton.edu>
2017-04-24 18:37:02 +0000

    Changed eboard position to char field

--- src/models.py
+++ src/models.py
@@ -128,7 +128,7 @@ class Slides(models.Model):
 
 class EBoard(models.Model):
     Name = models.CharField(max_length=50)
-    Position = models.IntegerField(choices=[(1, "President"), (2, "Co-President"), (3, "Treasurer"), (4, "Secretary"), (5, "Webmaster"), (6, "Treasurer/Secretary")])
+    Position = models.CharField(max_length=50)
 
     def get_absolute_url(self):
         return "/contact"
