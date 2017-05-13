9c7ec29c9828294eb7e5ef9006f32a5961f06521
Jeffrey Chan <jchan63@binghamton.edu>
2017-05-13 21:14:29 +0000

    Remove eboard table from reset script

--- reset.py
+++ reset.py
@@ -9,9 +9,8 @@ from src.models import Players
 from src.models import Matches
 from src.models import Practices
 from src.models import AttendanceHistory
-from src.models import EBoard
 
-response = raw_input("Are you sure you want to reset the current entries for Players, Matches, Practices, Attendance, and E-Board? (Y/N) ")
+response = raw_input("Are you sure you want to reset the current entries for Players, Matches, Practices, and Attendance? (Y/N) ")
 
 if response.upper() != 'Y':
     print "Reset aborted."
@@ -22,7 +21,6 @@ else:
         Matches.objects.all(),
         Practices.objects.all(),
         AttendanceHistory.objects.all(),
-        EBoard.objects.all(),
     ]
 
     for table in tables:
