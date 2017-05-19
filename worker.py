b3b61970afff3dd6f20360770885514206eb9c81
Jeffrey Chan <superjeffreyc.cs@gmail.com>
2017-05-19 17:06:12 -0400

    Changing back to Heroku scheduler. Remove infinite loop.

--- worker.py
+++ worker.py
@@ -1,18 +1,7 @@
 from work import *
-import time
 
 if __name__ == '__main__':
 
-	month = datetime.date.today().month
-
-	while True:
-		checkForMatchUpdates()
-		checkForPracticeUpdates()
-
-		# Delete log entries once a month
-		if month != datetime.date.today().month:
-			deleteLogEntries()
-
-		time.sleep(60)
-
-
+	checkForMatchUpdates()
+	checkForPracticeUpdates()
+	deleteLogEntries()
