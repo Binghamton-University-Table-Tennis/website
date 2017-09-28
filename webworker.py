007627ecce176c0d389418b08a0b9cfc054b9f9a
Jeffrey Chan <jchan63@binghamton.edu>
2017-09-28 01:53:03 +0000

    Clear database entries in July

--- webworker.py
+++ webworker.py
@@ -1,6 +1,14 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from work import *
+from src.models import Players
+from src.models import Matches
+from src.models import Practices
+from src.models import AttendanceHistory
+from django.contrib.admin.models import LogEntry
+from axes.utils import reset
+
 import os
+import datetime
 
 def run_web_script():
     os.system('gunicorn config.wsgi --log-file -')  # Runs forever
@@ -10,14 +18,42 @@ def start_scheduler():
     scheduler.add_job(doWork, 'interval', seconds=1)
     scheduler.start()   # Runs in the 'background'
 
+def clearEverything():
+    # Delete all django-axes lockouts and access records
+    reset()
+
+    # Erase all entries in these tables
+    tables = [
+        Players.objects.all(),
+        Matches.objects.all(),
+        Practices.objects.all(),
+        AttendanceHistory.objects.all(),
+        LogEntry.objects.all(),
+    ]
+
+    for table in tables:
+        for entry in table:
+            entry.delete()
+
 def doWork():
-    checkForMatchUpdates()
-    checkForPracticeUpdates()
-    deleteLogEntries()
+    '''
+    We need to clear the database entries once in a while so we don't go over the row limit in the Heroku Postgres database.
+    Therefore, we will reset all entries during July, since school is not in session.
+
+    Otherwise, update the attendances and matches as usual.
+    '''
+    if datetime.datetime.now().month == 7:
+        clearEverything()
+    else:
+        checkForMatchUpdates()
+        checkForPracticeUpdates()
+        deleteLogEntries()
 
 def run():
     start_scheduler()
     run_web_script()
+
+
 
 
 if __name__ == '__main__':
